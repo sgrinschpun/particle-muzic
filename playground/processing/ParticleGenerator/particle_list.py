8b15c798f32d08c9a1868f1aff9bff116dd67eef
sgrinschpun <sgrinschpun@ifae.es>
2018-05-29 13:28:05 +0200

    procesing_viz

--- playground/processing/ParticleGenerator/particle_list.py
+++ playground/processing/ParticleGenerator/particle_list.py
@@ -1,6 +1,6 @@
 e_params = {"name": "e-", "decay": [], "time_to_decay": "stable", "charge": -1.0, "mass": 0.0005109989461, "type": "lepton", "composition": []}
 pi_params = {"name": "pi-", "decay": ["mu-", "nubar_mu"], "time_to_decay": 6.745569150965853, "charge": -1.0, "mass": 0.13957018, "type": "meson", "composition": ["d", "ubar"]}
-n0_params = {"name": "n0", "decay": ["nubar_e", "e-", "p+"], "time_to_decay": 1.333699306732677, "charge": 0.0, "mass": 0.939565413, "type": "baryon", "composition": ["u", "d", "d"]}
+n0_params = {"name": "n0", "decay": [el], "time_to_decay": 1.333699306732677, "charge": 0.0, "mass": 0.939565413, "type": "baryon", "composition": ["u", "d", "d"]}
 mu_params={"name": "mu+", "decay": ["nu_e", "e+", "nubar_mu"], "time_to_decay": 3.9669850442559933, "charge": 1.0, "mass": 0.1056583745, "type": "lepton", "composition": []}
 tau_params={"name": "tau+", "decay": ["nubar_tau", "mu+", "nu_mu"], "time_to_decay": 3.692488723162409, "charge": 1.0, "mass": 1.77686, "type": "lepton", "composition": []}
 nu_e_params={"name": "nu_e", "decay": [], "time_to_decay": "stable", "charge": 0.0, "mass": 0.0, "type": "lepton", "composition": []}
