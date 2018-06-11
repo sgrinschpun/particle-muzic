0f7243b6199da353120e623b7ae3b4607a73116f
Cristobal Pio <cpio@pic.es>
2018-06-11 11:13:46 +0200

    Changing order, sending first add and then remove particle

--- python/phenomena/nodes/audiovideo_node.py
+++ python/phenomena/nodes/audiovideo_node.py
@@ -9,6 +9,7 @@ from phenomena.utils import get_logger
 
 class JsonRemoteAudioVideoNode(ParticleActionNodeChain):
     _IP =  "172.16.7.173"
+    #_IP = "127.0.0.1"
     _PORT = 1234
     def __init__(self):
         super(ParticleActionNodeChain, self).__init__()
@@ -39,8 +40,8 @@ class JsonRemoteAudioVideoNode(ParticleActionNodeChain):
             self._node.getNextNode(self).removeParticle(particle)
 
     def transformParticle(self, particle, new_particles):
-        rm_message = self._removeParticlesMessage([particle])
         add_message = self._addParticlesMessage(new_particles)
+        rm_message = self._removeParticlesMessage([particle])
         all_messages = rm_message + add_message
         self._node.getNextNode(self).transformParticle(particle, new_particles)
         #self._sendMessage(all_messages)
