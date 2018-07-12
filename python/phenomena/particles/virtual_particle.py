12e191bff78174020b9d5aa4c6cb83d0148ade9a
Sergi Masot Llima <40757242+sergimasot@users.noreply.github.com>
2018-07-12 16:20:25 +0200

    added virtual_particle

---
+++ python/phenomena/particles/virtual_particle.py
@@ -0,0 +1,120 @@
+from __future__ import division, print_function
+import math, random
+
+from particle_boosted import ParticleBoosted
+
+from particletools.tables import PYTHIAParticleData
+pythia = PYTHIAParticleData()
+
+NO_PARENT = -1
+class VirtualParticle(ParticleBoosted):
+
+    def __init__(self, name, parent = NO_PARENT, **kwargs):
+
+        if len(self.decay) == 3:
+            self._load_xml()
+        else:
+            pass
+
+
+    def _load_xml(self):
+        """Reads the xml and pics out particle data only. If no decay length
+        is given, it will calculated from the width."""
+
+        import xml.etree.ElementTree as ET
+        import os
+
+        base = os.path.dirname(os.path.abspath(__file__))
+        searchpaths = (base + '/ParticleData.xml', 'ParticleData.xml',
+                       '../ParticleData.xml',
+                       'ParticleDataTool/ParticleData.xml')
+        xmlname = None
+        for p in searchpaths:
+            if os.path.isfile(p):
+                xmlname = p
+                break
+        if xmlname is None:
+            raise IOError('ParticleDataTool::_load_xml(): '
+                          'XML file not found.')
+        root = ET.parse(xmlname).getroot()
+
+
+        k = pythia.pdg_id(self._name)
+        ids = [k]
+        ids.append(pythia.pdg_id(self.decay[0]))
+        ids.append(pythia.pdg_id(self.decay[1]))
+        ids.append(pythia.pdg_id(self.decay[2]))
+
+        #if k < 0:
+        #    ids *= -1
+        tags = [[str(ids[0]),str(ids[1])],
+            [str(ids[0]),str(-ids[2])],
+            [str(ids[0]),str(-ids[3])],
+            [str(ids[1]),str(ids[2])],
+            [str(ids[1]),str(ids[3])],
+            [str(ids[2]),str(ids[3])]]
+        tagsR = [[str(ids[1]),str(ids[0])],
+            [str(-ids[2]),str(ids[0])],
+            [str(-ids[3]),str(ids[0])],
+            [str(ids[2]),str(ids[1])],
+            [str(ids[3]),str(ids[1])],
+            [str(ids[3]),str(ids[2])]]
+        tagsbar = [[str(-ids[0]), str(-ids[1])],
+            [str(-ids[0]),str(ids[2])],
+            [str(-ids[0]),str(ids[3])],
+            [str(-ids[1]),str(-ids[2])],
+            [str(-ids[1]),str(-ids[3])],
+            [str(-ids[2]),str(-ids[3])]]
+        tagsbarR = [[str(-ids[0]), str(-ids[1])],
+            [str(ids[2]),str(-ids[0])],
+            [str(ids[3]),str(-ids[0])],
+            [str(-ids[2]),str(-ids[1])],
+            [str(-ids[3]),str(-ids[1])],
+            [str(-ids[3]),str(-ids[2])]]
+        combs = {'01':[], '02':[], '03':[], '12':[], '13':[], '23':[]}
+
+        for parent in root:
+            if parent.tag == 'particle':
+                for channel in parent:
+                    if channel.attrib['products'] == ' '.join(tags[0]) or channel.attrib['products'] == ' '.join(tagsR[0]):
+                        combs['01'].append(parent.attrib['name'])
+                    if channel.attrib['products'] == ' '.join(tags[1]) or channel.attrib['products'] == ' '.join(tagsR[1]):
+                        combs['02'].append(parent.attrib['name'])
+                    if channel.attrib['products'] == ' '.join(tags[2]) or channel.attrib['products'] == ' '.join(tagsR[2]):
+                        combs['03'].append(parent.attrib['name'])
+                    if channel.attrib['products'] == ' '.join(tags[3]) or channel.attrib['products'] == ' '.join(tagsR[3]):
+                        combs['12'].append(parent.attrib['name'])
+                    if channel.attrib['products'] == ' '.join(tags[4]) or channel.attrib['products'] == ' '.join(tagsR[4]):
+                        combs['13'].append(parent.attrib['name'])
+                    if channel.attrib['products'] == ' '.join(tags[5]) or channel.attrib['products'] == ' '.join(tagsR[5]):
+                        combs['23'].append(parent.attrib['name'])
+
+                    if channel.attrib['products'] == ' '.join(tagsbar[0]) or channel.attrib['products'] == ' '.join(tagsbarR[0]):
+                        combs['01'].append(pythia.pdg_id(parent.attrib['antiName']))
+                    if channel.attrib['products'] == ' '.join(tagsbar[1]) or channel.attrib['products'] == ' '.join(tagsbarR[1]):
+                        combs['02'].append(parent.attrib['antiName'])
+                    if channel.attrib['products'] == ' '.join(tagsbar[2]) or channel.attrib['products'] == ' '.join(tagsbarR[2]):
+                        combs['03'].append(parent.attrib['antiName'])
+                    if channel.attrib['products'] == ' '.join(tagsbar[3]) or channel.attrib['products'] == ' '.join(tagsbarR[3]):
+                        combs['12'].append(parent.attrib['antiName'])
+                    if channel.attrib['products'] == ' '.join(tagsbar[4]) or channel.attrib['products'] == ' '.join(tagsbarR[4]):
+                        combs['13'].append(parent.attrib['antiName'])
+                    if channel.attrib['products'] == ' '.join(tagsbar[5]) or channel.attrib['products'] == ' '.join(tagsbarR[5]):
+                        combs['23'].append(parent.attrib['antiName'])
+
+        print(combs)
+        print('\n')
+        print(set(combs['01']).intersection(combs['23']))
+        print(set(combs['02']).intersection(combs['13']))
+        print(set(combs['03']).intersection(combs['12']))
+
+        print('\n')
+        print(self.decay)
+
+
+        print('Fiesta!')
+
+    def _channelchoice(self):
+
+        self._load_xml()
+        
