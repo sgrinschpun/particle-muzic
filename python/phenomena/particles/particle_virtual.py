f7496485e7de488be129c1e9c2d1e0905cb9798c
Angilmuyor <40767194+Angilmuyor@users.noreply.github.com>
2018-07-12 16:51:15 +0200

    3 change PV

--- python/phenomena/particles/particle_virtual.py
+++ python/phenomena/particles/particle_virtual.py
@@ -77,33 +77,33 @@ class ParticleVirtual(ParticleBoosted):
             if parent.tag == 'particle':
                 for channel in parent:
                     if channel.attrib['products'] == ' '.join(tags[0]) or channel.attrib['products'] == ' '.join(tagsR[0]):
-                        combs['01'].append({'name':parent.attrib['name'], 'mass':parent.attrib['m0']})
+                        combs['01'].append([parent.attrib['name'],parent.attrib['m0']])
                     if channel.attrib['products'] == ' '.join(tags[1]) or channel.attrib['products'] == ' '.join(tagsR[1]):
-                        combs['02'].append({'name':parent.attrib['name'], 'mass':parent.attrib['m0']})
+                        combs['02'].append([parent.attrib['name'],parent.attrib['m0']])
                     if channel.attrib['products'] == ' '.join(tags[2]) or channel.attrib['products'] == ' '.join(tagsR[2]):
-                        combs['03'].append({'name':parent.attrib['name'], 'mass':parent.attrib['m0']})
+                        combs['03'].append([parent.attrib['name'],parent.attrib['m0']])
                     if channel.attrib['products'] == ' '.join(tags[3]) or channel.attrib['products'] == ' '.join(tagsR[3]):
-                        combs['12'].append({'name':parent.attrib['name'], 'mass':parent.attrib['m0']})
+                        combs['12'].append([parent.attrib['name'],parent.attrib['m0']])
                     if channel.attrib['products'] == ' '.join(tags[4]) or channel.attrib['products'] == ' '.join(tagsR[4]):
-                        combs['13'].append({'name':parent.attrib['name'], 'mass':parent.attrib['m0']})
+                        combs['13'].append([parent.attrib['name'],parent.attrib['m0']])
                     if channel.attrib['products'] == ' '.join(tags[5]) or channel.attrib['products'] == ' '.join(tagsR[5]):
-                        combs['23'].append({'name':parent.attrib['name'], 'mass':parent.attrib['m0']})
+                        combs['23'].append([parent.attrib['name'],parent.attrib['m0']])
 
                     if channel.attrib['products'] == ' '.join(tagsbar[0]) or channel.attrib['products'] == ' '.join(tagsbarR[0]):
-                        combs['01'].append({'name':parent.attrib['antiName'], 'mass':parent.attrib['m0']})
+                        combs['01'].append([parent.attrib['antiName'],parent.attrib['m0']])
                     if channel.attrib['products'] == ' '.join(tagsbar[1]) or channel.attrib['products'] == ' '.join(tagsbarR[1]):
-                        combs['02'].append({'name':parent.attrib['antiName'], 'mass':parent.attrib['m0']})
+                        combs['02'].append([parent.attrib['antiName'],parent.attrib['m0']])
                     if channel.attrib['products'] == ' '.join(tagsbar[2]) or channel.attrib['products'] == ' '.join(tagsbarR[2]):
-                        combs['03'].append({'name':parent.attrib['antiName'], 'mass':parent.attrib['m0']})
+                        combs['03'].append([parent.attrib['antiName'],parent.attrib['m0']])
                     if channel.attrib['products'] == ' '.join(tagsbar[3]) or channel.attrib['products'] == ' '.join(tagsbarR[3]):
-                        combs['12'].append({'name':parent.attrib['antiName'], 'mass':parent.attrib['m0']})
+                        combs['12'].append([parent.attrib['antiName'],parent.attrib['m0']])
                     if channel.attrib['products'] == ' '.join(tagsbar[4]) or channel.attrib['products'] == ' '.join(tagsbarR[4]):
-                        combs['13'].append({'name':parent.attrib['antiName'], 'mass':parent.attrib['m0']})
+                        combs['13'].append([parent.attrib['antiName'],parent.attrib['m0']])
                     if channel.attrib['products'] == ' '.join(tagsbar[5]) or channel.attrib['products'] == ' '.join(tagsbarR[5]):
-                        combs['23'].append({'name':parent.attrib['antiName'], 'mass':parent.attrib['m0']})
+                        combs['23'].append([parent.attrib['antiName'],parent.attrib['m0']])
 
 
-        print(set(combs['01']).intersection(combs['23']))
+        print(set(combs['01']['name']).intersection(combs['23']))
         print(set(combs['02']).intersection(combs['13']))
         print(set(combs['03']).intersection(combs['12']))
 
