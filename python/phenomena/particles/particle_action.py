537b43d1ff6a016b9472dff14486d29ef9bcb628
savalless <savalless@users.noreply.github.com>
2018-07-16 13:00:51 +0200

    Deleting the parent tag

--- python/phenomena/particles/particle_action.py
+++ python/phenomena/particles/particle_action.py
@@ -120,7 +120,7 @@ class ParticleAccumulatorNode(ParticleActionNodeChain):
                 name = new_particle['name']
                 kwargs = {'theta': new_particle['theta'], 'p': new_particle['p']}
                 #print "{0} ".format(ParticleBoosted(name, parent = particle.id, theta = theta, p = p)),
-                tr_new_particles.append(ParticleBoosted(name, parent = particle.id, **kwargs))
+                tr_new_particles.append(ParticleBoosted(name, particle.id, **kwargs))
             print " New particles: "
             for new_particle in new_particles: print new_particle['name'], new_particle['p'], new_particle['theta']
             self._node.getNextNode(self).transformParticle(particle, tr_new_particles)
