bc145ef96a7c34fde1eb66af9f40b3f14c4ff180
sgrinschpun <sgrinschpun@ifae.es>
2018-10-15 22:18:26 +0200

    passing tests

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -2,10 +2,12 @@ import pytest
 import path
 import math
 
-from phenomena.particles.particle_boosted import ParticleBoosted
-from phenomena.particles.particletest import ParticleTest
+from phenomena.particles.models import BubbleChamberParticle, QuantumUniverseParticle
 #imports for test_fetchers
 from phenomena.particles.sources import ParticleDataSource, ParticleDataToolFetcher, SciKitHEPFetcher, DecayLanguageFetcher, ExtraInfoFetcher
+
+#Which Particle Model to Use
+PARTICLE = BubbleChamberParticle
 
 #precision
 @pytest.fixture(scope='session',)
@@ -17,26 +19,26 @@ def resolution():
 @pytest.fixture(scope='function')
 def particle_rest(particle):
     '''Returns particle at rest'''
-    return ParticleTest(particle)
+    return PARTICLE(particle)
 
 #boosted particle
 @pytest.fixture(scope='function')
 def particle_boosted(particle,momentum):
     '''Returns boosted particle with given momentum'''
-    return  ParticleTest(particle, p=momentum)
+    return  PARTICLE(particle, p=momentum)
 
 #decay values particle
 @pytest.fixture(scope='function')
 def particle_decay_values(particle,momentum):
     '''Returns decay values of boosted particle with given momentum'''
-    return  ParticleTest(particle, momentum).decayvalues
+    return  PARTICLE(particle, momentum).decayvalues
 
 #conservations
 @pytest.fixture(scope='function')
 def particle_conservation_energy(particle,momentum):
     '''Returns energy in and energy out'''
     energy_dict ={}
-    original_particle = ParticleTest(particle, momentum)
+    original_particle = PARTICLE(particle, momentum)
     energy_dict["energy_in"] = original_particle.E
     energy_out = 0.
     for part in original_particle.decayvalues:
@@ -48,16 +50,16 @@ def particle_conservation_energy(particle,momentum):
 def particle_conservation_momentum(particle,momentum):
     '''Returns momentum in and momentum out, both t and l'''
     momentum_dict ={}
-    original_particle = ParticleTest(particle,momentum)
-    momentum_dict["pt_in"] = original_particle.p*math.sin(original_particle.theta)
-    momentum_dict["pl_in"] = original_particle.p*math.cos(original_particle.theta)
+    original_particle = PARTICLE(particle,momentum)
+    momentum_dict["pt_in"] = original_particle.p*math.sin(original_particle.phi)
+    momentum_dict["pl_in"] = original_particle.p*math.cos(original_particle.phi)
     pt_out = 0.
     for part in original_particle.decayvalues:
-        pt_out += part["p"]*math.sin(part['theta'])
+        pt_out += part["p"]*math.sin(part['phi'])
     momentum_dict["pt_out"]=pt_out
     pl_out = 0.
     for part in original_particle.decayvalues:
-        pl_out += part["p"]*math.cos(part['theta'])
+        pl_out += part["p"]*math.cos(part['phi'])
     momentum_dict["pl_out"]=pl_out
     return momentum_dict
 
@@ -65,11 +67,11 @@ def particle_conservation_momentum(particle,momentum):
 def particle_conservation_charge(particle,momentum):
     '''Returns charge in and charge out'''
     charge_dict = {}
-    original_particle = ParticleTest(particle, momentum)
+    original_particle = PARTICLE(particle, momentum)
     charge_dict['charge_in'] = original_particle.charge
     charge_out = 0.
     for part in original_particle.decayvalues:
-        charge_out += ParticleBoosted(part['name']).charge
+        charge_out += PARTICLE.getcharge(part['name'])
     charge_dict['charge_out'] = charge_out
     return charge_dict
 
