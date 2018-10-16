95bc9fbf45230a2e73cc4128667beb19a8eb6aff
sgrinschpun <sgrinschpun@ifae.es>
2018-10-16 15:57:08 +0200

    particle extra info loads file with pkg_resources

--- playground/mixins/mixins.py
+++ playground/mixins/mixins.py
@@ -3,10 +3,12 @@ import math
 import path
 
 from phenomena.particles.models import BubbleChamberParticle
-from phenomena.particles.particle_server import ParticleServer
+from phenomena.particles.server_particle import ServerParticle
 
 if __name__ == '__main__':
     #pi = ParticleServer.init('pi+', p=1, phi=1.)
     #print (pi.mass)
     pi = BubbleChamberParticle('pi+', p=1, phi=1.)
     print (pi.mass)
+    print (pi.pdgid)
+    print (pi.composition)
