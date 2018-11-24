9879c223930fe889391fa18c8a32f6cdc9a57327
sgrinschpun <sgrinschpun@ifae.es>
2018-11-24 15:56:24 +0100

    decaythroughvirtual work in progress

--- python/phenomena/particles/transformations/types/decays/decaytransformation.py
+++ python/phenomena/particles/transformations/types/decays/decaytransformation.py
@@ -13,14 +13,10 @@ class Decay(Transformation):
         self._buildTransfValues()
 
     def _outputParticles(self):
-        return Transformation.channelListToNames(self._particle.decay_channels)
+        return Transformation.channelListToNames(VirtualDecay(self._particle.decay_channels)
 
     def getProbability(self, dt=1./60.):
         dt = dt *u.s
         FACTOR = 1e8
         probability = 1- math.exp(-1*dt/self._particle.lifetime * u.picosecond / u.s)
         return probability * FACTOR
-
-class DecayThroughVirtual(Transformation):
-    pass
-    pass
