8b1ffaff6b124ba705d9ac2d3cdf42e3c5fa6adb
sgrinschpun <sgrinschpun@ifae.es>
2018-11-30 19:40:42 +0100

    wip

--- python/phenomena/particles/transformations/transformationchannel.py
+++ python/phenomena/particles/transformations/transformationchannel.py
@@ -1,8 +1,63 @@
+from collections import namedtuple
 
+from phenomena.particles.sources import ParticleDataSource
 
+Channel = namedtuple('Channel', 'BR particles')
 
 class TransformationChannel(object):
-    pass
+    def __init__(self, BR, particles):
+        self._item = Channel(BR,particles)
+
+    @property
+    def BR(self):
+        return self._item.BR
+
+    @property
+    def ids(self):
+        return self._item.particles
+
+    @property
+    def names(self):
+        return map(ParticleDataSource.getName,self.ids )
+
+    def len(self):
+        return len(self.ids)
+
+    def nameSet(self):
+        return set(self.names)
+
+    def idSet(self):
+        return set(self.ids)
+
+    def totalCharge(self):
+        charge = 0.
+        for name in self.names:
+            charge += ParticleDataSource.getCharge(name)
+
+    leptonDict= [{
+    'e-':1,'nu_e':1,
+    'e+':-1,'nu_ebar':-1},{
+    'mu-':1,'nu_mu':1,
+    'mu+':-1,'nu_mubar':-1},{
+    'tau-': 1,'nu_tau':1,
+    'tau+':-1,'nu_taubar':-1
+    }]
+
+    LeptonNumber = namedtuple('LeptonNumber', 'total, list')
+
+    def leptonNumber(self):
+        leptonnumber = [0,0,0]
+        for index, family in enumerate(TransformationChannel.leptonDict):
+            for name in self.names:
+                try:
+                    leptonnumber[index] += leptonDict[name]
+                except:
+                    leptonnumber[index] += 0
+
+        return LeptonNumber(sum(leptonnumber),leptonnumber)
+
+    def isLeptonNeutrino(self):
+        
 
 
 class TransformationChannels(object):
